d16d6894a42327b86e51d58733821129ce43108e
yangshindo <eliwood182@hotmail.com>
2021-06-29 14:41:08 -0300

    Commit #2

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
 const mongoose = require("mongoose");
 const app = express();
 
@@ -15,115 +14,132 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static("public"));
 
-const uri = process.env.MONGO_ATLAS;
-mongoose.connect("mongodb+srv://yangshindo:hurricaneman18@cluster0.majo4.mongodb.net/wikiDB", {useNewUrlParser:true,useUnifiedTopology:true});
+//reservado para credenciais do MongoDB Atlas salvas no .env
+//const uri = process.env.MONGO_ATLAS;
 
-const articleSchema = {
+//host local da database
+mongoose.connect("'mongodb://localhost:27017/WikiDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+//Definindo Schema e Model utilizando Mongoose para integrar MongoDB ao Node.
+
+const articleSchema = new mongoose.Schema({
   title: String,
   content: String
-};
+});
 
 const Article = mongoose.model("Article", articleSchema);
 
 
-///////////////////////////////////Requests Targetting all Articles////////////////////////
+///Resquests Gerais///
 
 app.route("/articles")
 
-.get(function(req, res){
-  Article.find(function(err, foundArticles){
-    if (!err) {
-      res.send(foundArticles);
-    } else {
-      res.send(err);
-    }
+  .get(function(req, res) {
+    Article.find(function(err, foundArticles) {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+
+  .post(function(req, res) {
+
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content
+    });
+
+    newArticle.save(function(err) {
+      if (!err) {
+        res.send("Successfully added a new article.");
+      } else {
+        res.send(err);
+      }
+    });
+  })
+
+  .delete(function(req, res) {
+
+    Article.deleteMany(function(err) {
+      if (!err) {
+        res.send("Successfully deleted all articles.");
+      } else {
+        res.send(err);
+      }
+    });
   });
-})
 
-.post(function(req, res){
-
-  const newArticle = new Article({
-    title: req.body.title,
-    content: req.body.content
-  });
-
-  newArticle.save(function(err){
-    if (!err){
-      res.send("Successfully added a new article.");
-    } else {
-      res.send(err);
-    }
-  });
-})
-
-.delete(function(req, res){
-
-  Article.deleteMany(function(err){
-    if (!err){
-      res.send("Successfully deleted all articles.");
-    } else {
-      res.send(err);
-    }
-  });
-});
-
-////////////////////////////////Requests Targetting A Specific Article////////////////////////
+///Resquests para artigo específico///
 
 app.route("/articles/:articleTitle")
 
-.get(function(req, res){
+  .get(function(req, res) {
 
-  Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
-    if (foundArticle) {
-      res.send(foundArticle);
-    } else {
-      res.send("No articles matching that title was found.");
-    }
+    Article.findOne({
+      title: req.params.articleTitle
+    }, function(err, foundArticle) {
+      if (foundArticle) {
+        res.send(foundArticle);
+      } else {
+        res.send("No articles matching that title was found.");
+      }
+    });
+  })
+
+  .put(function(req, res) {
+
+    Article.update({
+        title: req.params.articleTitle
+      }, {
+        title: req.body.title,
+        content: req.body.content
+      }, {
+        overwrite: true
+      },
+      function(err) {
+        if (!err) {
+          res.send("Successfully updated the selected article.");
+        }
+      }
+    );
+  })
+
+  .patch(function(req, res) {
+
+    Article.update({
+        title: req.params.articleTitle
+      }, {
+        $set: req.body
+      },
+      function(err) {
+        if (!err) {
+          res.send("Successfully updated article.");
+        } else {
+          res.send(err);
+        }
+      }
+    );
+  })
+
+  .delete(function(req, res) {
+
+    Article.deleteOne({
+        title: req.params.articleTitle
+      },
+      function(err) {
+        if (!err) {
+          res.send("Successfully deleted the corresponding article.");
+        } else {
+          res.send(err);
+        }
+      }
+    );
   });
-})
-
-.put(function(req, res){
-
-  Article.update(
-    {title: req.params.articleTitle},
-    {title: req.body.title, content: req.body.content},
-    {overwrite: true},
-    function(err){
-      if(!err){
-        res.send("Successfully updated the selected article.");
-      }
-    }
-  );
-})
-
-.patch(function(req, res){
-
-  Article.update(
-    {title: req.params.articleTitle},
-    {$set: req.body},
-    function(err){
-      if(!err){
-        res.send("Successfully updated article.");
-      } else {
-        res.send(err);
-      }
-    }
-  );
-})
-
-.delete(function(req, res){
-
-  Article.deleteOne(
-    {title: req.params.articleTitle},
-    function(err){
-      if (!err){
-        res.send("Successfully deleted the corresponding article.");
-      } else {
-        res.send(err);
-      }
-    }
-  );
-});
 
 
 
---
+++ readme.txt
@@ -0,0 +1,7 @@
+Projeto de API normas REST
+Javascript, Mongoose, Node, Express.
+
+Realizei esse projeto com a finalidade de criar um API RESTful simples com banco
+de dados MONGO com possibilidade de hostear na cloud MongoAtlas ou local.
+Os testes da API foram executados pelo postman (postman.com). Backend em Node,
+routing pelo Express.
